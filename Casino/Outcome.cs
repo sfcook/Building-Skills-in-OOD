072d776abe1903afcf4261256cb7542129a4202b
sfcook <stephenfostercook@gmail.com>
2017-03-12 19:46:37 -0500

    Added Outcome class

---
+++ Casino/Outcome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Outcome
+    {
+        public string name { get; set; }
+        public int odds { get; set; }
+
+        public Outcome(string name, int odds)
+        {
+            this.name = name;
+            this.odds = odds;
+        }
+    }
+}
