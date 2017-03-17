6f87a2125a1a32fda054a4ed7f3145dcf28d30fb
sfcook <stephenfostercook@gmail.com>
2017-03-16 21:05:52 -0500

    Added Wheel class

---
+++ Casino/Wheel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Wheel
+    {
+        public List<Bin<Outcome>> bins { get; set; }
+        public Random rng { get; set; }
+
+        Wheel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddOutcome(int number, Outcome outcome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bin<Outcome> Next()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bin<Outcome> Get(int bin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
