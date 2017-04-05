25cfcda3d7577480ad2689093f624d6c17f7541b
sfcook <stephenfostercook@gmail.com>
2017-04-04 20:48:25 -0500

    Added Bet class and BetTest

---
+++ Casino/Bet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Bet
+    {
+        public int amountBet;
+        public Outcome outcome;
+
+        public Bet()
+        {
+
+        }
+
+        public int WinAmount()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int LoseAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CasinoTests/BetTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CasinoTests
+{
+    [TestClass]
+    public class BetTest
+    {
+        [TestMethod]
+        public void BetConstructor()
+        {
+        }
+
+        [TestMethod]
+        public void BetWinAmount()
+        {
+        }
+
+        [TestMethod]
+        public void BetLoseAmount()
+        {
+        }
+
+        [TestMethod]
+        public void BetToString()
+        {
+        }
+    }
+}
