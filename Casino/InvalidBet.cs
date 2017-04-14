a45d861d39aeb8aaadfa238cedf185b178f39acf
sfcook <stephenfostercook@gmail.com>
2017-04-14 18:48:45 -0500

    Completed InvalidBet

---
+++ Casino/InvalidBet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Casino
+{
+    /// <summary>
+    /// Raised when Player attempts to place an invalid bet.
+    /// </summary>
+    public class InvalidBet : Exception
+    {
+    }
+}
---
+++ CasinoTests/InvalidBetTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Casino;
+
+namespace CasinoTests
+{
+    [TestClass]
+    public class InvalidBetTest
+    {
+        [TestMethod]
+        public void InvalidBetException()
+        {
+            try
+            {
+                throw new InvalidBet();
+            }
+            catch(Exception ex)
+            {
+                Assert.AreEqual(ex.GetType(), typeof(InvalidBet));
+            }
+        }
+    }
+}
