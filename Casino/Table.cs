e61399d7233ef7d3319d8aedf75bd72804dca3ed
sfcook <stephenfostercook@gmail.com>
2017-04-14 19:06:57 -0500

    Added Table class

---
+++ Casino/Table.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Casino
+{
+    public class Table : IEnumerable<Bet>
+    {
+        public int limit;
+        public int minimum;
+        public List<Bet> bets;
+
+        public Table()
+        {
+
+        }
+
+        public void PlaceBet(Bet bet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IsValid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Bet> GetEnumerator()
+        {
+            return ((IEnumerable<Bet>)bets).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Bet>)bets).GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
